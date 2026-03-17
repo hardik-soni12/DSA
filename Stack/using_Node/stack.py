a6710f521d8383eaaf1e53265cdeb58417e03ea1
hardik-soni12 <imailhr1k@gmail.com>
2026-03-17 14:01:29 +0530

    add feature printing and size

--- Stack/using_Node/stack.py
+++ Stack/using_Node/stack.py
@@ -6,3 +6,19 @@ class Stack:
         self.__n = 0
 
     
+    # size and printing
+
+    def size(self):
+        '''
+        returns the size of the stack
+        '''
+
+        return self.__n
+    
+    def __str__(self) :
+        curr = self.__top
+        res = []
+        while curr:
+            res.append(str(curr.data))
+            curr = curr.next
+        return ' '.join(res)
