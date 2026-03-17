f499546f4e0f6bc05bb991cdc81c8419550e43e8
hardik-soni12 <imailhr1k@gmail.com>
2026-03-17 13:07:56 +0530

    add feature-deletion

--- LinkedList/Doubly-LL/doubly-ll.py
+++ LinkedList/Doubly-LL/doubly-ll.py
@@ -84,7 +84,69 @@ class DoublyLinkedList:
                     curr.next = new_node
                 
         self.__n += 1
+
+
+    # deletion methods
+    def pop(self, item=None):
+        '''
+        Removes the specified item if present else the last item if no argument is given.
+        Returns the data of the removed node.
+        '''
+
+        if self.__head is None:
+            raise IndexError('pop on empty linked list')
+        
+        # if item is None remove last element
+        curr = self.__head
+        if item is None:
+            while curr.next != None:
+                curr = curr.next
+        
+        else:
+            # if item is given serach for it
+            while curr != None and curr.data != item:
+                curr = curr.next
+
+            if curr is None:
+                raise ValueError('item not found')
+            
+        removed_data = curr.data
+        if curr == self.__head:
+            self.__head = curr.next
+            if self.__head:
+                self.__head.prev = None
+
+        
+        else:
+            curr.prev.next = curr.next
+            if curr.next:
+                curr.next.prev = curr.prev
+        
+        if curr == self.__tail:
+            self.__tail = curr.prev
+            
+        self.__n -= 1
+        return removed_data
     
+
+    
+    def delete_head(self):
+        '''
+        removes the head node of a Linked list and returns the removed item
+        '''
+
+        if self.__head is None:
+            raise IndexError('delete head on empty linked list')
+        
+        removed_item = self.__head.data
+        self.__head = self.__head.next
+        if self.__head:
+            self.__head.prev = None
+        self.__n -= 1
+        return removed_item
+
+
+
 
 d = DoublyLinkedList()
 d.append(10)
@@ -94,5 +156,7 @@ d.insert_head(5)
 d.append(13)
 d.insertAfter(5,6)
 print((d))
+d.pop()
+d.delete_head()
 
-print(len(d))
+print((d))
