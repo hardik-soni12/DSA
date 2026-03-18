4dd3c9a78a74af4fc500e9910a93bb270597e04b
hardik-soni12 <imailhr1k@gmail.com>
2026-03-18 11:16:45 +0530

    add feature printing and traverse

--- Stack/using_array/stack.py
+++ Stack/using_array/stack.py
@@ -14,8 +14,28 @@ class Stack:
         self.top+=1
         self.__items[self.top] = value
 
+    
+    # printing and traverse
+
+    def __str__(self) -> str:
+        res = []
+        for data in self.__items:
+            if data:
+                res.append(str(data))
+        
+        return ' '.join(res)
+    
+
+    def traverse(self):
+        if self.__items:
+            for i in range(len(self.__items)-1,-1, -1):
+                if self.__items[i]:
+                    print(self.__items[i])
+            
+
 
 s = Stack(5)
-s.push(10)
-s.push(20)
-
+# s.push(10)
+# s.push(20)
+print(s)
+s.traverse()
