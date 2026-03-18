eec39e637e5fb4a0917e8165b80e0e933bf7eff4
hardik-soni12 <imailhr1k@gmail.com>
2026-03-18 10:55:42 +0530

    add feature insertion

--- Stack/using_array/stack.py
+++ Stack/using_array/stack.py
@@ -5,4 +5,17 @@ class Stack:
         self.top = -1
 
 
-    
+    # insertion
+
+    def push(self, value):
+        '''inserts item from the top returns None'''
+        if self.top == self.size-1:
+            return 'stack Overflow'
+        self.top+=1
+        self.__items[self.top] = value
+
+
+s = Stack(5)
+s.push(10)
+s.push(20)
+
