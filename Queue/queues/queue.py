24a60748a6dc16d3efb21ebed74e48b2cf38c00d
hardik-soni12 <imailhr1k@gmail.com>
2026-03-19 09:27:32 +0530

    add feature len and isEmpty methods

--- Queue/queues/queue.py
+++ Queue/queues/queue.py
@@ -5,4 +5,16 @@ class Queue:
         self.__front = self.__rear = None
         self.__n = 0
 
-        
+
+    def __len__(self):
+        return self.__n
+    
+
+    def isEmpty(self):
+        '''returns True of queue is Empty else returns False'''
+
+        if self.__front:
+            return True
+        return False
+    
+
