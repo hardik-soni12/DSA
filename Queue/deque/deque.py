9eea1837f2962f893b51c9469e2f80060a52e578
hardik-soni12 <imailhr1k@gmail.com>
2026-03-20 12:02:30 +0530

    add feature deletion

--- Queue/deque/deque.py
+++ Queue/deque/deque.py
@@ -86,13 +86,63 @@ class Deque:
 
         self.__n += 1
 
+
+    # deletion methods
+
+    def RemoveFront(self):
+        '''removes item from the front and returns removed item'''
+
+        if self.isEmpty():
+            raise IndexError('Remove front on empty queue')
+        
+        removed_item = self.__front.data
+        if self.__front == self.__rear:
+            self.__front = self.__rear = None
+
+        else:
+            self.__front = self.__front.next
+            self.__front.prev = None
+
+        self.__n -= 1
+        return removed_item
+    
+
+    def RemoveRear(self):
+        '''Remove items from the Rear and returns the removed item'''
+
+        if self.isEmpty():
+            raise IndexError('Remove Rear on empty queue')
+        
+        removed_item = self.__rear.data
+        if self.__front == self.__rear:
+            self.__front = self.__rear = None
+
+        else:
+            self.__rear = self.__rear.prev
+            self.__rear.next = None
+        
+        self.__n -= 1
+        return removed_item
+
+
 d = Deque()
 d.AddFront(0)
 d.AddFront(1)
 d.AddFront(2)
 d.AddRear(-1)
 d.AddRear(-2)
-print(d)
+print('before remove',d)
 print('length',len(d))
 print('peek from front',d.PeekFront())
 print('peek from rear',d.PeekRear())
+print('-'*20)
+d.RemoveFront()
+d.RemoveRear()
+print('after remove front',d)
+print('length',len(d))
+print('peek from front',d.PeekFront())
+print('peek from rear',d.PeekRear())
+
+print('-'*20)
+for i in d:
+    print(i)
