5870055f358e77c468130698559d7603f50a559a
hardik-soni12 <imailhr1k@gmail.com>
2026-03-20 10:54:20 +0530

    add feature len and isEmpty

--- Queue/dequeue/dequeue.py
+++ Queue/dequeue/dequeue.py
@@ -4,4 +4,14 @@ class Dequeue:
     def __init__(self):
         self.__front = self.__rear = None
         self.__n = 0
-        
+    
+    def __len__(self):
+        return self.__n
+    
+    def isEmpty(self):
+        '''returns True if queue is empty, else returns False'''
+        if self.__front:
+            return False
+        return True
+    
+    
