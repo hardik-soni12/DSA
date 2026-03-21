7d9398528a6aa27a66326420c785f4172a62bd74
hardik-soni12 <imailhr1k@gmail.com>
2026-03-21 13:29:26 +0530

    add feature isEmpty and isFull

--- Queue/CircularQueue/circularQueue.py
+++ Queue/CircularQueue/circularQueue.py
@@ -4,4 +4,18 @@ class CircularQueue:
         self.array = [None]*size
         self._front = self.__rear = -1
     
+
+    def isEmpty(self):
+        '''returns True if queue is Empty'''
+        return self._front == -1
     
+    def isFull(self):
+        '''returns True if queue is full'''
+        return (self.__rear + 1) % self.size == self._front
+    
+    
+    
+
+c = CircularQueue(5)
+
+print(c.isFull())
